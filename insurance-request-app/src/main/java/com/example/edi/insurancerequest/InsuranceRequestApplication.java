e5c30d07fde83d76ff61e08f37e8f27ac755a628
Angel Martin <apmartinolivera@gmail.com>
2026-03-29 00:10:51 -0400

    feat: add StAEDI, Testcontainers deps and interchange config to insurance-request-app

--- insurance-request-app/src/main/java/com/example/edi/insurancerequest/InsuranceRequestApplication.java
+++ insurance-request-app/src/main/java/com/example/edi/insurancerequest/InsuranceRequestApplication.java
@@ -1,9 +1,12 @@
 package com.example.edi.insurancerequest;
 
+import com.example.edi.insurancerequest.config.InterchangeProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(scanBasePackages = {"com.example.edi.insurancerequest", "com.example.edi.common"})
+@EnableConfigurationProperties(InterchangeProperties.class)
 public class InsuranceRequestApplication {
     public static void main(String[] args) {
         SpringApplication.run(InsuranceRequestApplication.class, args);
